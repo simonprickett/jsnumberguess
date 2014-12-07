30e6b2d3392bd156245f8dcff9b092b844d930b0
Simon Prickett <simon@crudworks.org>
2014-12-07 10:48:55 -0800

    Changed title on index.html, moved JavaScript to its own file.

--- js/numberguess.js
+++ js/numberguess.js
@@ -1,1 +1,40 @@
-// TODO
+function writeMessage(elementId, message, appendMessage) {
+	var elemToUpdate = document.getElementById(elementId);
+	if (appendMessage) {
+		elemToUpdate.innerHTML = elemToUpdate.innerHTML + message;
+	} else {
+		elemToUpdate.innerHTML = message;
+	}
+};
+
+function getNewSecretNumber() {
+	return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+};
+
+window.onload = function() {
+	var secretNumber = getNewSecretNumber();
+	var guessBtn = document.getElementById('buttonArea');
+
+	guessBtn.addEventListener('click', function() {
+		var userGuessed = document.getElementById('userguess').value;
+		var statusArea = document.getElementById('statusArea');
+		var historyList = document.getElementById('historyList');
+
+		if (userGuessed == secretNumber) {
+			// Got it
+			writeMessage('statusArea', '<p>You got me I was thinking ' + secretNumber + ', let\'s go again...</p>');
+			writeMessage('historyList', '<ul></ul>');
+			secretNumber = getNewSecretNumber();
+		} else if (userGuessed < secretNumber) {
+			// User needs to guess higher
+			writeMessage('statusArea', '<p>You need to guess higher than ' + userGuessed + '</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' (too low)</li>', true);
+		} else {
+			// User needs to guess lower
+			writeMessage('statusArea', '<p>You need to guess lower than ' + userGuessed + '</p>');
+			writeMessage('historyList', '<li>' + userGuessed + ' (too high)</li>', true);
+		}
+
+		document.getElementById('userguess').value = '';
+	});
+};
